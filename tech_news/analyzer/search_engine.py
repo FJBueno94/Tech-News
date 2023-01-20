418266a331880fb3aa92a81109ec3e85f60a778b
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-20 14:25:16 -0300

    fix search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,12 @@
 from tech_news.database import search_news
-import datetime
+from datetime import datetime
+
+
+def data_validation(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # https://stackoverflow.com/questions/26699885/how-can-i-use-a-regex-variable-in-a-query-for-mongodb
@@ -15,22 +22,18 @@ def search_by_title(title):
 # https://stackoverflow.com/questions/969285/how-do-i-translate-an-iso-8601-datetime-string-into-a-python-datetime-object
 # Requisito 7
 def search_by_date(date):
-    try:
-        search = search_news(
-            {
-                "timestamp": {
-                    "$eq": datetime.datetime.fromisoformat(date).strftime(
-                        "%d-%m-%Y"
-                    )
-                }
+    data_validation(date)
+    search = search_news(
+        {
+            "timestamp": {
+                "$eq": datetime.fromisoformat(date).strftime("%d/%m/%Y")
             }
-        )
-        response = []
-        for new in search:
-            response.append((new["title"], new["url"]))
-        return response
-    except ValueError:
-        raise ValueError("Data inválida")
+        }
+    )
+    response = []
+    for new in search:
+        response.append((new["title"], new["url"]))
+    return response
 
 
 # Requisito 8
